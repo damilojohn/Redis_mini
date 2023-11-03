65bd3bed18ebc20b2cfcd55137fa8ba4a8ca9e8a
Damilola-John <Oduguwadamilola40@gmail.com>
2023-11-03 17:44:21 +0100

    added client object

--- redis.py
+++ redis.py
@@ -18,7 +18,7 @@ class ProtocolHandler(object):
             '-': self.error_handler,
             ':': self.int_handler,
             '$': self.binary_handler,
-            '*': self.arr_handler,
+            '*': self.array_handler,
             '%': self.dict_handler,    
         }
     def handle_request(self,socket_file):
@@ -60,8 +60,8 @@ class ProtocolHandler(object):
     
     
     def write_response(self,socket_file,data):
-        # serialize the response data and send it to the client
-        buf = BytesIO 
+        # serializes client request
+        buf = BytesIO()
         self._write(buf,data)
         buf.seek(0)
         socket_file.write(buf.getvalue())
@@ -72,22 +72,22 @@ class ProtocolHandler(object):
             data = data.encode('utf-8')
             
         if isinstance(data,bytes):
-            buf.write('$%s\r\n%s\r\n' % (len(data),data))
+            buf.write('$%s/r/n%s/r/n' % (len(data),data))
         
         elif isinstance(data,int):
-            buf.write('#%s\r\n' % data)
+            buf.write('#%s/r/n' % data)
         
         elif isinstance(data,(list,tuple)):
-            buf.write('*%s\r\n' % len(data))
+            buf.write('*%s/r/n' % len(data))
             for item in data :
                 self._write(buf,item)
         elif isinstance(data, dict):
-            buf.write('%%%s\r\n' % len(data))
+            buf.write('%%%s/r/n' % len(data))
             for key in data:
                 self._write(buf,key)
                 self._write(buf,data[key])
         elif data is None:
-            buf.write('$-1\r\n')
+            buf.write('$-1/r/n')
         else:
             raise CommandError('unrecognized type: %s' % type(data))
 
@@ -139,7 +139,7 @@ class Server(object):
             raise CommandError('missing command')
         command = data[0].upper()
         if command not in self._commands:
-            raise CommandError('Unrecognized command: %s' command)
+            raise CommandError('Unrecognized command: %s' % command)
         return self._commands[command](*data[1:])
     
     def get(self,key):
@@ -173,8 +173,46 @@ class Server(object):
     def run(self):
         self._server.serve_forever()
             
-                    
-                    
+
+class Client(object):
+    def __init__(self,host='127.0.0.1',port=31337):
+        self._protocol = ProtocolHandler()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((host,port))
+        self._fh = self._socket.makefile('rwb')
+    
+    def execute(self,*args):
+        self._protocol.write_response(self._fh,args)
+        resp = self._protocol.handle_request(self._fh)
+        
+        if isinstance(resp,Error):
+            raise CommandError(resp.message)
+        return resp
+       
+    def get(self,key):
+        return self.execute('GET',key)
+    
+    def set(self,key):
+        return self.execute('SET',key)
+    
+    def delete(self, key):
+        return self.execute('DELETE',key)
+    
+    def flush(self):
+        return self.execute('FLUSH')
+    
+    def mget(self, *keys):
+        return self.execute('MGET',*keys)
+    
+    def mset(self,*items):
+        return self.execute('MSET',*items)
+
+
+if __name__ == '__main__':
+    from gevent import monkey; monkey.patch_all()
+    Server().run()
+    
+                 
             
             
                 
